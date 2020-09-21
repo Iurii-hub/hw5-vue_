89c1dec00758454ed2ffb6904ffaefaf694c4603
Iurii-hub <st.play31@gmail.com>
2020-09-21 17:42:17 +0300

    HW5

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    
+
+    <!-- Давайте спробуємо переробити наш тудуліст з використанням Vuex В state в нас має бути масив todo: [] 
+    В actions в нас має бути 3 метода remove, add, get 
+    Змінюємо масив todo через mutations
+    В getters можемо добавити метод який буде перевіряти довжину нашого масиву.
+     -->
+
+     
   </div>
 </template>
 
